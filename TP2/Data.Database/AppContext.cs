162d22a6af95313ee92e54bf5bb0ae5995e565f3
Alejandro <alejandrorecalde5@gmail.com>
2021-06-08 16:57:53 -0300

    Add entity fw and update everything to net5

---
+++ TP2/Data.Database/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using Business.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Database
+{
+    class AppContext: DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Especialidad> Especialidades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => 
+            options.UseSqlite(@"Data Source=C:\Users\aleja\OneDrive\Documentos\Net2021-GM02\TP2");
+    }
+}
