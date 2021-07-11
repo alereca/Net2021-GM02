130c02bfe8aa9b18aa6a58c99e97e89cb5d35aab
Alejandro <alejandrorecalde5@gmail.com>
2021-07-11 19:04:26 -0300

    Get conn string from config file

--- TP2/Data.Database/AcademyContext.cs
+++ TP2/Data.Database/AcademyContext.cs
@@ -2,18 +2,22 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.Database
 {
-    class AcademyContext: DbContext
-    {
-        public DbSet<Usuario>? Usuarios { get; set; }
-        public DbSet<Especialidad>? Especialidades { get; set; }
+class AcademyContext: DbContext
+{
+    public DbSet<Usuario>? Usuarios { get; set; }
+    public DbSet<Especialidad>? Especialidades { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options) => 
-            options.UseSqlite(@"Data Source=C:\Users\aleja\OneDrive\Documentos\Net2021-GM02\TP2\academia.db");
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        string connString = ConfigurationManager.ConnectionStrings["ConnStringLocal"].ConnectionString;
+        options.UseSqlite(connString);
     }
+}
 }
