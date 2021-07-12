2a4d0e0e5dd359221e736c391e9ade555f54f10d
Alejandro <alejandrorecalde5@gmail.com>
2021-07-12 15:41:12 -0300

    Get one user ado net sql server version

--- TP2/Data.Database/UsuarioAdapter.cs
+++ TP2/Data.Database/UsuarioAdapter.cs
@@ -1,6 +1,8 @@
 using Business.Entities;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,8 +29,28 @@ namespace Data.Database
 
         public Business.Entities.Usuario GetOne(int id)
         {
-            _adapter.OpenConnection();
+            var usr = new Usuario();
 
-        }
+            string connString = ConfigurationManager.ConnectionStrings["ConnStringLocal"].ConnectionString;
+            var sqlConnection = new SqlConnection(connString);
+            sqlConnection.Open();
+            var cmdGetOneUser = new SqlCommand("select * from Usuarios as U where U.id = @id", sqlConnection);
+            cmdGetOneUser.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = id;
+            SqlDataReader drGetOne = cmdGetOneUser.ExecuteReader();
+            if (drGetOne.Read())
+            {
+                usr.ID = (int)drGetOne["ID"];
+                usr.Nombre = (string)drGetOne["NombreUsuario"];
+                usr.Clave = (string)drGetOne["Clave"];
+                usr.Habilitado = (bool)drGetOne["Habilitado"];
+                usr.Nombre = (string)drGetOne["Nombre"];
+                usr.Apellido = (string)drGetOne["Apellido"];
+                usr.Email = (string)drGetOne["Email"];
+            }
+            drGetOne.Close();
+            sqlConnection.Close();
+
+            return usr;
+        } 
     }
 }
